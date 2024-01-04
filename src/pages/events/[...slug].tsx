7454deb64c3393670ed170f213c5972c3c37a5b0
himanshudube97 <himanshu.dube13@gmail.com>
2024-01-04 23:55:23 +0530

    first

---
+++ src/pages/events/[...slug].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilteredEventsPage = () => {
+    return (
+        <div>
+            slug
+        </div>
+    );
+};
+
+export default FilteredEventsPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,5 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Link from "next/link";
-//Thiw is the root page->>>>
-const inter = Inter({ subsets: ["latin"] });
-
+import { getFeaturedEvents } from "../../dummy-data";
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      Hello World
-      <ul>
-        <Link href="/about">About</Link>
-        <Link href="/clients">Clients</Link>
-      </ul>
-    </main>
-  );
+  const featuredEvents = getFeaturedEvents();
+  return <div></div>;
 }
